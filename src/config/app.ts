48201617fd9413f472ebbb286c8ff1d2754eeb73
김진영 <skswls0@daum.net>
2025-04-13 23:23:54 +0900

    fix: Base URL 변경

--- src/config/app.ts
+++ src/config/app.ts
@@ -4,8 +4,8 @@ export const appConfig = {
     'https://play.google.com/store/apps/details?id=site.tobehealthy.webview&pcampaignid=web_share',
   appStoreLink:
     'https://apps.apple.com/kr/app/%EA%B1%B4%EA%B0%95%ED%95%B4%EC%A7%90/id6547838272',
-  termsLink: 'https://main.to-be-healthy.site/policy/terms',
-  privacyLink: 'https://main.to-be-healthy.site/policy/privacy',
+  termsLink: 'https://main.to-be-healthy.shop/policy/terms',
+  privacyLink: 'https://main.to-be-healthy.shop/policy/privacy',
 };
 
 export type AppConfig = typeof appConfig;
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: '건강해짐',
   name: '건강해짐',
-  url: 'https://www.to-be-healthy.site',
+  url: 'https://www.to-be-healthy.shop',
   ogImage: '/images/banner.png',
   description: '피트니스 센터, 트레이너와 회원을 위한 필수 앱',
   locale: 'ko_KR',
